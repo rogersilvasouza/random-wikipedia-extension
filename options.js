9e93690defedc58e3df0f43dfce607c3730e9913
Roger Silva Souza <rogersilvasouza@hotmail.com>
2014-10-07 15:05:56 -0300

    Issue #1 página de option
    Local storage e save e restore option

---
+++ options.js
@@ -0,0 +1,46 @@
+/*jslint browser: true*/
+/*global $, jQuery, console, alert, GET, chrome, language*/
+// Saves options to chrome.storage
+
+
+if (Storage === "undefined") {
+    console.log('Local Storage Not Suported');
+} else {
+    console.log('Local Storage Suport');
+}
+
+var saveButton = document.getElementById('save'), language;
+
+function restoreLanguage() {
+
+    "use strict";
+
+    language = localStorage.getItem("language");
+
+    document.getElementById("language").value = language;
+
+}
+
+function saveLanguage() {
+
+    "use strict";
+
+    localStorage.setItem("language", document.getElementById("language").value);
+
+}
+
+saveButton.addEventListener('click', function () {
+
+    "use strict";
+
+    saveLanguage();
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    "use strict";
+
+    restoreLanguage();
+
+});
